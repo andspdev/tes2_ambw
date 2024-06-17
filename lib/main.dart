970a11ab4aca4c65538837e213dc93ae21529726
Andreaas Pandu P <andspemail@gmail.com>
2024-06-17 20:40:56 +0700

    Update Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:tes2_ambw/fragment/Beranda.dart';
-import 'package:tes2_ambw/fragment/Profil.dart';
-import 'package:tes2_ambw/fragment/baca_nanti.dart';
-import 'package:tes2_ambw/fragment/kategori.dart';
+import 'dart:async';
+
+import './main_home.dart';
 
 void main() {
-  runApp(MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatefulWidget 
-{
-  @override
-  _MainApp createState() => _MainApp();
-}
+class MyApp extends StatelessWidget {  
+  const MyApp({super.key});
 
-class _MainApp extends State<MainApp> 
-{
-  int _selectedTab = 0;
-  String titleApp = 'Beranda';
+  @override  
+  Widget build(BuildContext context) {  
+    return const MaterialApp(  
+      home: MyHomePage(),  
+      debugShowCheckedModeBanner: false,  
+    );  
+  }  
+}  
 
-  final List<Widget> _pages = [
-    const Beranda(),
-    const Kategori(),
-    const BacaNanti(),
-    const Profil()
-  ];
+class MyHomePage extends StatefulWidget {  
+  const MyHomePage({super.key});
 
-  final List<String> _title = [
-    'Beranda',
-    'Kategori',
-    'Baca Nanti',
-    'Profil'
-  ];
+  @override  
+  SplashScreenState createState() => SplashScreenState();  
+}  
 
-  _changeTab(int index) {
-    setState(() {
-      _selectedTab = index;
-      titleApp = _title[index];
-    });
-  }
+class SplashScreenState extends State<MyHomePage> {  
+  @override  
+  void initState() {  
+    super.initState();  
+    Timer(const Duration(seconds: 3),  
+        () => Navigator.pushReplacement(context,  
+        MaterialPageRoute(builder:  
+          (context) => MainHome()  
+        )
+      )  
+    );
+  }  
 
-  @override
-  Widget build(BuildContext context) {
-
+  @override  
+  Widget build(BuildContext context) {  
     return MaterialApp(
-      title: '$titleApp | Test 2 AMBW',
+      title: 'Test 2 AMBW',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(scaffoldBackgroundColor: const Color.fromARGB(255, 255, 255, 255)),
-      home: Scaffold(
-        body: _pages[_selectedTab],
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _selectedTab,
-          backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-          onTap: (index) => _changeTab(index),
-          mouseCursor: SystemMouseCursors.grab,
-          iconSize: 20,
-          unselectedFontSize: 12,
-          selectedFontSize: 12,
-          type: BottomNavigationBarType.fixed,
-          unselectedItemColor: const Color.fromARGB(255, 72, 72, 72),
-          selectedIconTheme: const IconThemeData(color: Color.fromRGBO(6, 99, 220, 1)),
-          selectedItemColor: const Color.fromRGBO(6, 99, 220, 1),
-          selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
-          unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home_outlined),
-              label: 'Beranda'
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.menu),
-              label: 'Kategori'
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.bookmark_border),
-              label: 'Baca Nanti'
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person_outline),
-              label: 'Profil'
-            ),
-          ],
-        ),
-      ),
+      home: Scaffold()
     );
-  }
+  }  
 }
---
+++ lib/main_home.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+import './fragment/Beranda.dart';
+import './fragment/Profil.dart';
+import './fragment/baca_nanti.dart';
+import './fragment/kategori.dart';
+
+
+class MainHome extends StatefulWidget 
+{
+  @override
+  _MainHome createState() => _MainHome();
+}
+
+class _MainHome extends State<MainHome> 
+{
+  int _selectedTab = 0;
+  String titleApp = 'Beranda';
+
+  final List<Widget> _pages = [
+    const Beranda(),
+    const Kategori(),
+    const BacaNanti(),
+    const Profil()
+  ];
+
+  final List<String> _title = [
+    'Beranda',
+    'Kategori',
+    'Baca Nanti',
+    'Profil'
+  ];
+
+  _changeTab(int index) {
+    setState(() {
+      _selectedTab = index;
+      titleApp = _title[index];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: '$titleApp | Test 2 AMBW',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(scaffoldBackgroundColor: const Color.fromARGB(255, 255, 255, 255)),
+      home: Scaffold(
+        body: _pages[_selectedTab],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedTab,
+          backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+          onTap: (index) => _changeTab(index),
+          mouseCursor: SystemMouseCursors.grab,
+          iconSize: 20,
+          unselectedFontSize: 12,
+          selectedFontSize: 12,
+          type: BottomNavigationBarType.fixed,
+          unselectedItemColor: const Color.fromARGB(255, 72, 72, 72),
+          selectedIconTheme: const IconThemeData(color: Color.fromRGBO(6, 99, 220, 1)),
+          selectedItemColor: const Color.fromRGBO(6, 99, 220, 1),
+          selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+          unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              label: 'Beranda'
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.menu),
+              label: 'Kategori'
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.bookmark_border),
+              label: 'Baca Nanti'
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline),
+              label: 'Profil'
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
