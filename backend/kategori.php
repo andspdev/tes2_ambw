9ede5b4ec248d32b2081dc4d83331a9da9f3dd81
Andreaas Pandu P <andspemail@gmail.com>
2024-06-18 16:37:51 +0700

    Update API daftar kategori

--- backend/kategori.php
+++ backend/kategori.php
@@ -1,3 +1,54 @@
 <?php
 
+header('Content-Type: application/json');
+
 include './includes/config.php';
+
+
+if (strtolower($_SERVER['REQUEST_METHOD']) == 'get')
+{
+    $status_code = 200;
+
+    $list_kategori = $pdo->prepare(
+        "SELECT
+            id,
+            nama
+        FROM kategori
+        WHERE deleted_at IS NULL
+        ORDER BY nama ASC
+        "
+    );
+    $list_kategori->execute();
+
+    $kategori_arr = [];
+    while($kategori = $list_kategori->fetch(PDO::FETCH_OBJ))
+        $kategori_arr[] = [
+            'id' => htmlspecialchars($kategori->id),
+            'nama' => htmlspecialchars($kategori->nama)
+        ];
+
+    $output = [
+        'kategori' => $kategori_arr
+    ];
+}
+else
+{
+    $status_code = 400;
+
+    $output = [
+        'messages' => [
+            'errors' => 'Method tidak didukung!'
+        ]
+    ];
+}
+
+
+$output = array_merge([
+    'status' => $status_code,
+    'error' => $status_code
+], $output);
+
+
+echo json_encode($output, JSON_PRETTY_PRINT);
+
+$pdo = null;
