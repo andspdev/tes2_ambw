684e17ed5114d0d7e91e3e33b9d5f09d54f4d1a8
Andreaas Pandu P <andspemail@gmail.com>
2024-06-19 08:25:13 +0700

    API Baca Nanti / Bookmark

--- backend/baca_nanti.php
+++ backend/baca_nanti.php
@@ -1,3 +1,66 @@
 <?php
 
+header('Content-Type: application/json');
+
 include './includes/config.php';
+
+
+if (strtolower($_SERVER['REQUEST_METHOD']) == 'get')
+{
+    $status_code = 200;
+
+    $user_id = $_GET['user_id'] ?? null;
+    $select_berita = $pdo->prepare(
+        "SELECT
+            br.id, 
+            br.judul, 
+            br.created_at,
+            br.thumbnail,
+            k.nama as nama_kategori
+        FROM bookmark as bk
+        INNER JOIN berita as br
+        ON br.id = bk.berita_id
+        LEFT JOIN kategori as k
+        ON k.id = br.kategori_id AND k.deleted_at IS NULL
+        WHERE br.deleted_at IS NULL
+        ORDER BY br.created_at DESC"
+    );
+    $select_berita->execute();
+
+    $berita_terbaru = [];
+    while($berita = $select_berita->fetch(PDO::FETCH_OBJ))
+        $berita_terbaru[] = [
+            'id' => htmlspecialchars($berita->id),
+            'judul_berita' => htmlspecialchars($berita->judul),
+            'thumbnail' => htmlspecialchars($berita->thumbnail),
+            'dibuat_pada' => htmlspecialchars($berita->created_at),
+            'nama_kategori' => htmlspecialchars($berita->nama_kategori)
+        ];
+
+    $output = [
+        'berita' => $berita_terbaru
+    ];
+}
+else
+{
+    $status_code = 400;
+
+    $output = [
+        'messages' => [
+            'errors' => 'Method tidak didukung!'
+        ]
+    ];
+}
+
+
+$output = array_merge([
+    'status' => $status_code,
+    'error' => $status_code
+], $output);
+
+
+http_response_code($status_code);
+echo json_encode($output, JSON_PRETTY_PRINT);
+
+
+$pdo = null;
