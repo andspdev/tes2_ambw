49bdcdf1c3df5b6185d4e52da5c6f96dd11142eb
Andreaas Pandu P <andspemail@gmail.com>
2024-06-19 08:07:52 +0700

    API Detail berita (done)

--- backend/detail.php
+++ backend/detail.php
@@ -1,3 +1,76 @@
 <?php
 
+header('Content-Type: application/json');
+
 include './includes/config.php';
+
+
+if (strtolower($_SERVER['REQUEST_METHOD']) == 'get')
+{
+    $status_code = 200;
+    $id = $_GET['id'] ?? null;
+
+    $fetch_detail = $pdo->prepare(
+        "SELECT
+            judul,
+            deskripsi,
+            views,
+            thumbnail,
+            b.created_at as dibuat_pada,
+            k.nama as nama_kategori,
+            k.id as kategori_id
+        FROM `berita` as b
+        LEFT JOIN `kategori` as k
+        ON h.id = b.kategori_id
+        WHERE deleted_at IS NULL AND id = ?"
+    );
+
+    $fetch_detail->execute([ $id ]);
+
+    if ($fetch_detail->rowCount() > 0)
+    {
+        $fetch_detail = $fetch_detail->fetch(PDO::FETCH_OBJ);
+
+        $output = [
+            'detail_berita' => [
+                'judul' => htmlspecialchars($fetch_detail->judul),
+                'deskripsi' => htmlspecialchars($fetch_detail->deskripsi),
+                'views' => htmlspecialchars($fetch_detail->views),
+                'dibuat_pada' => htmlspecialchars($fetch_detail->dibuat_pada),
+                'kategori' => [
+                    'id' => htmlspecialchars($fetch_detail->kategori_id),
+                    'nama' => htmlspecialchars($fetch_detail->nama_kategori)
+                ],
+            ]
+        ];
+    }
+    else
+    {
+        $output = [
+            'messages' => [
+                'errors' => 'Berita tidak ditemukan.'
+            ]
+        ];
+    }
+}
+else
+{
+    $status_code = 400;
+
+    $output = [
+        'messages' => [
+            'errors' => 'Method tidak didukung!'
+        ]
+    ];
+}
+
+
+$output = array_merge([
+    'status' => $status_code,
+    'error' => $status_code
+], $output);
+
+
+echo json_encode($output, JSON_PRETTY_PRINT);
+
+$pdo = null;
